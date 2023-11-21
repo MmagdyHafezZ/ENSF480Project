24f6bc01d23912f39ccca9fe76f33ff3213fc4c4
Magdy Hafez <Magdy.hafez9123@gmail.com>
2023-11-21 13:52:25 -0700

    tweaks

--- FlightWeb/src/Components/Sign/Sign.jsx
+++ FlightWeb/src/Components/Sign/Sign.jsx
@@ -182,26 +182,24 @@ const SignUpSignIn = () => {
 
   return (
     <div className="body-signup">
-      <div id="background-wrap">
-        <div class="x1">
-          <div class="cloud"></div>
-        </div>
+      <div class="x1">
+        <div class="cloud"></div>
+      </div>
 
-        <div class="x2">
-          <div class="cloud"></div>
-        </div>
+      <div class="x2">
+        <div class="cloud"></div>
+      </div>
 
-        <div class="x3">
-          <div class="cloud"></div>
-        </div>
+      <div class="x3">
+        <div class="cloud"></div>
+      </div>
 
-        <div class="x4">
-          <div class="cloud"></div>
-        </div>
+      <div class="x4">
+        <div class="cloud"></div>
+      </div>
 
-        <div class="x5">
-          <div class="cloud"></div>
-        </div>
+      <div class="x5">
+        <div class="cloud"></div>
       </div>
 
       <div className="main">
