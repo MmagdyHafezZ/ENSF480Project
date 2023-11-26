eebbf1412be0c3577b373d9ff86ec884e53c2ef8
dietban19 <dieter.banaag@ucalgary.ca>
2023-11-26 15:47:33 -0700

    fix: nav

--- Frontend/FlightWeb/src/App.jsx
+++ Frontend/FlightWeb/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home.jsx";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import SeatsPage from "./pages/SeatsPage/SeatsPage.jsx";
-import Sign from "./pages/Sign/Sign.jsx";
+// import Sign from "./pages/Sign/Sign.jsx";
 const App = () => {
   return (
     <>
