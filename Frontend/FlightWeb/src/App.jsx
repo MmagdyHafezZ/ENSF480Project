6a0fbd69be0ef30267269bd3cee09c2794318729
dietban19 <dieter.banaag@ucalgary.ca>
2023-11-26 15:47:07 -0700

    fix: nav

--- Frontend/FlightWeb/src/App.jsx
+++ Frontend/FlightWeb/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Home from "./pages/Home/Home.jsx";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-
+import SeatsPage from "./pages/SeatsPage/SeatsPage.jsx";
+import Sign from "./pages/Sign/Sign.jsx";
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/seats" element={<SeatsPage />} />
       </Routes>
     </>
   );
