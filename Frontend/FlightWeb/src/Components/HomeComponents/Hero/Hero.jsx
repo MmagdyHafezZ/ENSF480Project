1be0c07afcafe954181297a99d776b8785b534dc
Magdy Hafez <Magdy.hafez9123@gmail.com>
2023-11-26 21:06:32 -0700

    changes

--- Frontend/FlightWeb/src/Components/HomeComponents/Hero/Hero.jsx
+++ Frontend/FlightWeb/src/Components/HomeComponents/Hero/Hero.jsx
@@ -11,7 +11,13 @@ const Hero = () => {
       <div className="hero-register-wrapper sectionContainer">
         <div className="hero-register-container">
           <h4>Register With Us Today</h4>
-          <button>Register</button>
+          <button
+            onClick={() => {
+              window.location.href = "/signin";
+            }}
+          >
+            Register
+          </button>
         </div>
       </div>
       <div className="hero-wrapper">
