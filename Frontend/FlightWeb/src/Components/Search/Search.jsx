7d9f3e872e44bad7afbfcb471bb664e813ea5179
Magdy Hafez <Magdy.hafez9123@gmail.com>
2023-11-26 20:03:21 -0700

    small tweaks

--- Frontend/FlightWeb/src/Components/Search/Search.jsx
+++ Frontend/FlightWeb/src/Components/Search/Search.jsx
@@ -1,17 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import {HiOutlineLocationMarker} from 'react-icons/hi'
-import {RiAccountPinCircleLine} from 'react-icons/ri'
-import {RxCalendar} from 'react-icons/rx'
+// Import icons
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { RxCalendar } from "react-icons/rx";
 
 const Search = () => {
+  // State for form data
+  const [formData, setFormData] = useState({
+    location: "",
+    travelers: "",
+    checkIn: "",
+    checkOut: "",
+  });
+
+  // Function to handle input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const holdColor = () => {
+    const btns = document.querySelectorAll(".singleBtn");
+    btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        btns.forEach((btn) => {
+          btn.classList.remove("active");
+        });
+        btn.classList.add("active");
+      });
+    });
+  };
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("YOUR_BACKEND_ENDPOINT", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Handle success
+      console.log("Form data submitted:", formData);
+      // Reset form or navigate to another page, etc.
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
   return (
     <div className="search container section">
-        <div className="sectionContainer grid">
-
+      <form onSubmit={handleSubmit} className="sectionContainer grid">
+        <div
+          className="sectionContainer grid"
+          style={{
+            cursor: "pointer",
+          }}
+        >
           <div className="btns flex">
-
-            <div className="singleBtn">
+            <div className="singleBtn" onClick={holdColor()}>
               <span>Economy</span>
             </div>
             <div className="singleBtn">
@@ -26,51 +85,59 @@ const Search = () => {
             {/* Single Input */}
             <div className="singleInput flex">
               <div className="iconDiv">
-                <HiOutlineLocationMarker className="icon"/>
+                <HiOutlineLocationMarker className="icon" />
               </div>
               <div className="texts">
                 <h4>Location</h4>
-                <input type="text" placeholder="Where we dropping boys?"/>
+                <input
+                  name="location"
+                  type="text"
+                  placeholder="Where we dropping boys?"
+                  value={formData.location}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
 
             {/* Single Input */}
             <div className="singleInput flex">
               <div className="iconDiv">
-                <RiAccountPinCircleLine className="icon"/>
+                <RiAccountPinCircleLine className="icon" />
               </div>
               <div className="texts">
                 <h4>Travelers</h4>
-                <input type="text" placeholder="Add Dudes"/>
+                <input type="text" placeholder="Add Dudes" />
               </div>
             </div>
 
             {/* Single Input */}
             <div className="singleInput flex">
               <div className="iconDiv">
-                <RxCalendar className="icon"/>
+                <RxCalendar className="icon" />
               </div>
               <div className="texts">
                 <h4>Check In</h4>
-                <input type="text" placeholder="Add Date"/>
+                <input type="text" placeholder="Add Date" />
               </div>
             </div>
 
             {/* Single Input */}
             <div className="singleInput flex">
               <div className="iconDiv">
-                <RxCalendar className="icon"/>
+                <RxCalendar className="icon" />
               </div>
               <div className="texts">
                 <h4>Check out</h4>
-                <input type="text" placeholder="Add Date"/>
+                <input type="text" placeholder="Add Date" />
               </div>
             </div>
-          <button className='btn btnBlock flex'>Search Flight</button>
+            <button type="submit" className="btn btnBlock flex">
+              Search Flight
+            </button>
           </div>
         </div>
+      </form>
     </div>
-
-  )
-}
-export default Search
+  );
+};
+export default Search;
