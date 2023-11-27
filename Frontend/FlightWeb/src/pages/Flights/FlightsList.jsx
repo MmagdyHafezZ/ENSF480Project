6717d8e405231a714031841189a9c76d69615f0c
dietban19 <dieter.banaag@ucalgary.ca>
2023-11-27 11:19:11 -0700

    added flights list containers

---
+++ Frontend/FlightWeb/src/pages/Flights/FlightsList.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./flights.scss";
+const flightsData = [
+  {
+    "Air Canada": {
+      departingTime: "4:30pm",
+      arrivingTime: "7:30pm",
+      departureLocation: "Calgary, AB",
+      arrivalLocation: "Vancouver, BC",
+      leavingTime: "2:30pm",
+      arriveBackTime: "5:30pm",
+      leavingLocation: "Vancouver, BC",
+      arriveBackLocation: "Calgary, AB",
+      price: "89$",
+    },
+  },
+  {
+    "United Airlines": {
+      departingTime: "6:00pm",
+      arrivingTime: "9:00pm",
+      departureLocation: "Los Angeles, CA",
+      arrivalLocation: "New York, NY",
+      leavingTime: "2:30pm",
+      arriveBackTime: "5:30pm",
+      leavingLocation: "Vancouver, BC",
+      arriveBackLocation: "Calgary, AB",
+
+      price: "120$",
+    },
+  },
+  {
+    "British Airways": {
+      departingTime: "10:00am",
+      arrivingTime: "2:00pm",
+      departureLocation: "London, UK",
+      arrivalLocation: "Paris, FR",
+      leavingTime: "2:30pm",
+      arriveBackTime: "5:30pm",
+      leavingLocation: "Vancouver, BC",
+      arriveBackLocation: "Calgary, AB",
+      price: "75£",
+    },
+  },
+  // Add more airline objects here with their respective details
+];
+const FlightsList = () => {
+  return (
+    <>
+      {flightsData.map((flight, index) => {
+        const airline = Object.keys(flight)[0]; // Get the airline name
+        const details = flight[airline]; // Get the details for this airline
+
+        return (
+          <div key={index} className="flight-details-container">
+            <div className="flight-details__times">
+              <div className="flight-details__time-info">
+                <div className="flights-info-left">
+                  <div className="plane-company">{airline}</div>
+                  <span>Departing</span>
+                </div>
+                <div className="flights-info-right">
+                  <div className="flight-time-window">
+                    <span>
+                      {details.departingTime} - {details.arrivingTime}
+                    </span>
+                  </div>
+                  <div className="flight-depart-return">
+                    {details.departureLocation} - {details.arrivalLocation}
+                  </div>
+                </div>
+              </div>
+              <div className="flight-details__time-info">
+                <div className="flights-info-left">
+                  <div className="plane-company">{airline}</div>
+                  <span>Returning</span>
+                </div>
+                <div className="flights-info-right">
+                  <div className="flight-time-window">
+                    <span>
+                      {details.leavingTime} - {details.arriveBackTime}
+                    </span>
+                  </div>
+                  <div className="flight-depart-return">
+                    {details.leavingLocation} - {details.arriveBackLocation}
+                  </div>
+                </div>
+              </div>
+              {/* Include Returning section if needed */}
+            </div>
+            <div className="flight-details__price">
+              <div className="flight-price">{details.price}</div>
+              <span>Round Trip</span>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default FlightsList;
--- Frontend/FlightWeb/src/pages/Flights/FlightsPage.jsx
+++ Frontend/FlightWeb/src/pages/Flights/FlightsPage.jsx
@@ -4,6 +4,7 @@ import { useUserDataContext } from "../../context/UserDataContext";
 import Navbar from "../../Components/Navbar/Navbar";
 import Search from "../../Components/HomeComponents/Search/Search";
 import "./flights.scss";
+import FlightsList from "./FlightsList";
 
 const FlightsPage = () => {
   const { userFlightData, setUserFlightData } = useUserDataContext();
@@ -78,8 +79,16 @@ const FlightsPage = () => {
   return (
     <>
       <Navbar />
-      <div className="flights-container flex container">
+      <div className="flights-wrapper flex container">
         <Search />
+        <div className="flights-main-container flex">
+          {/* <div className="flights-main__left">Left</div> */}
+          <div className="flights-list__container">
+            {/* List of Flights Here */}
+            <FlightsList />
+          </div>
+          <div className="flights-main__right">Right</div>
+        </div>
       </div>
     </>
   );
