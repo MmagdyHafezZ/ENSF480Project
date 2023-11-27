0ba94bcce4e619ee7cb49e7abdd1a1936454b3c0
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-11-27 08:39:15 -0700

    re set backend up for experience purposes

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package ENSF480.Backend;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightReservationSystemApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightReservationSystemApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package ENSF480.Backend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlightReservationSystemApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
