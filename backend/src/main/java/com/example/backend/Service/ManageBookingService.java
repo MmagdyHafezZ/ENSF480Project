37a40181dd5e54272409087620e6c3d60b60ba3a
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-11-28 12:12:52 -0700

    small edits

--- backend/src/main/java/com/example/backend/Service/ManageBookingService.java
+++ backend/src/main/java/com/example/backend/Service/ManageBookingService.java
@@ -1,7 +1,6 @@
 package com.example.backend.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,29 +16,30 @@ public class ManageBookingService {
 
     @Autowired
     private ManageBookingRepository manageBookingRepository;
-
-    // POST
-    public ManageBookingEntity postManageBookingData(ManageBookingEntity manageBookingEntity){
-        return manageBookingRepository.save(manageBookingEntity);
-    }
-
+    
     // GET
     public List<ManageBookingEntity> getManageBookingData(){
         return manageBookingRepository.findAll();
     }
-    
-    // DELETE
-    public void deleteManageBookingData(Long id){
-        manageBookingRepository.deleteById(id);
+
+    // POST
+    public ManageBookingEntity postManageBookingEntity(ManageBookingEntity manageBookingEntity){
+        return manageBookingRepository.save(manageBookingEntity);
     }
 
-    public ManageBookingEntity updateManageBookingData(Long id, ManageBookingEntity existingData){
+    // PUT
+    public ManageBookingEntity updateManageBookingEntity(Long id, ManageBookingEntity existingData){
         ManageBookingEntity updateData = manageBookingRepository.findById(id).get();
         updateData.setPassenger(existingData.getPassenger());
         updateData.setFlight(existingData.getFlight());
         updateData.setConfirm(existingData.getConfirm());
-
+        
         return manageBookingRepository.save(updateData);
+    }
+    
+    // DELETE
+    public void deleteManageBookingEntity(Long id){
+        manageBookingRepository.deleteById(id);
     }
 
 }
