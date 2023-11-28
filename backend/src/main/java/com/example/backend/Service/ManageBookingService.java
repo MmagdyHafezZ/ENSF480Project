a15da3cf51140d07811e4173ccbcbec4d70d7560
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-11-28 10:20:08 -0700

    booking service class

---
+++ backend/src/main/java/com/example/backend/Service/ManageBookingService.java
@@ -0,0 +1,30 @@
+package com.example.backend.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.Entity.ManageBookingEntity;
+import com.example.backend.Repository.ManageBookingRepository;
+
+/**
+ * ManageBookingService
+ */
+@Service
+public class ManageBookingService {
+
+    @Autowired
+    private ManageBookingRepository manageBookingRepository;
+
+    // POST
+    public ManageBookingEntity postManageBookingData(ManageBookingEntity manageBookingEntity){
+        return manageBookingRepository.save(manageBookingEntity);
+    }
+
+    // GET
+    public List<ManageBookingEntity> getManageBookingData(){
+        return manageBookingRepository.findAll();
+    }
+    
+}
