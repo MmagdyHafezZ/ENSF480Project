1ecd17e5bd003cbff4d2ae7ae0f89077bcad938e
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-11-28 09:57:49 -0700

    ManageBookingRepository interface created

---
+++ backend/src/main/java/com/example/backend/Repository/ManageBookingRepository.java
@@ -0,0 +1,13 @@
+package com.example.backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.backend.Entity.ManageBookingEntity;
+
+/**
+ * ManageBookingRepository
+ */
+public interface ManageBookingRepository extends JpaRepository<ManageBookingEntity, Integer>{
+
+    
+}
