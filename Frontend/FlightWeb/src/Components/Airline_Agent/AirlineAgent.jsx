bb00bae36095222cd12337abaabb495f4ba95be3
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2023-11-28 20:39:51 -0700

    edited so that PUT method works

--- Frontend/FlightWeb/src/Components/Airline_Agent/AirlineAgent.jsx
+++ Frontend/FlightWeb/src/Components/Airline_Agent/AirlineAgent.jsx
@@ -19,32 +19,48 @@ import {
   Box,
   Pagination,
 } from "@mui/material";
+import axios from "axios";
 
 const AirlineAgentPage = () => {
-  const [bookings, setBookings] = useState([
-    // Dummy data for demonstration
-    {
-      id: 1,
-      passengerName: "John Doe",
-      flight: "NY to LA",
-      status: "Confirmed",
-      details: "Seat 21A, No meal preference",
-    },
-    {
-      id: 2,
-      passengerName: "Jane Smith",
-      flight: "Berlin to Paris",
-      status: "Confirmed",
-      details: "Seat 14C, Vegetarian",
-    },
-    // Add more bookings here
-  ]);
+  // const [bookings, setBookings] = useState([
+  //   // Dummy data for demonstration
+  //   {
+  //     id: 1,
+  //     passengerName: "John Doe",
+  //     flight: "NY to LA",
+  //     status: "Confirmed",
+  //     details: "Seat 21A, No meal preference",
+  //   },
+  //   {
+  //     id: 2,
+  //     passengerName: "Jane Smith",
+  //     flight: "Berlin to Paris",
+  //     status: "Confirmed",
+  //     details: "Seat 14C, Vegetarian",
+  //   },
+  //   // Add more bookings here
+  // ]);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedBooking, setSelectedBooking] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const [openEditDialog, setOpenEditDialog] = useState(false);
   const [openCancelDialog, setOpenCancelDialog] = useState(false);
   const [agent, setAgent] = useState(localStorage.getItem("agent") || false);
+  const [getBookings, setGetBookings] = useState([]);
+  const [putPassenger, setPutPassenger] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/getBooking")
+      .then((response) => {
+        setGetBookings(response.data)
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      })
+  }, []);
+
   useEffect(() => {
     if (agent == false) {
       window.location.href = "/signin-agent";
@@ -66,6 +82,7 @@ const AirlineAgentPage = () => {
   const handleOpenEditDialog = (event, booking) => {
     event.stopPropagation(); // Prevent event from propagating to the table row
     setSelectedBooking(booking);
+    setPutPassenger(booking.passenger);
     setOpenEditDialog(true);
   };
 
@@ -83,9 +100,39 @@ const AirlineAgentPage = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredBookings = bookings.filter((booking) =>
-    booking.passengerName.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handlePassengerEdit = (event) => {
+    setPutPassenger(event.target.value);
+  }
+
+  const handlePutMethod = async() => {
+    try{
+      const response = await 
+        axios
+          .put(`http://localhost:8080/putBooking/${selectedBooking.id}`,
+            {
+              ...selectedBooking,
+              passenger: putPassenger
+            }
+          );
+      console.log(response.status);
+      console.log(response);
+      if(response.status === 200) {
+        const index = getBookings.findIndex((booking) => booking.id === selectedBooking.id)
+        let newBookings = [...getBookings];
+        newBookings[index] = {...newBookings[index], passenger: putPassenger}
+        setGetBookings(newBookings);
+        setOpenEditDialog(false);
+      } else {
+        console.log("Error updating booking: ", response.data)
+      }
+    } catch (error) {
+      console.error("Error updating booking: ", error);
+    }
+  }
+
+  // const filteredBookings = getBookings.filter((booking) =>
+  //   booking.passengerName.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
   const saveData = async () => {
     // Gather data from form fields
     const data = {};
@@ -140,6 +187,35 @@ const AirlineAgentPage = () => {
             </TableRow>
           </TableHead>
           <TableBody>
+            {getBookings.filter((booking) =>
+              booking.passenger.toLowerCase().includes(searchTerm.toLowerCase())
+            ).map((getBooking) => (
+              <TableRow
+                key={getBooking.id}
+                onClick={() => handleOpenDialog(getBooking)}
+                style={{ cursor: "pointer" }}
+              >
+                <TableCell>{getBooking.passenger}</TableCell>
+                <TableCell>{getBooking.flight}</TableCell>
+                <TableCell>{getBooking.confirm}</TableCell>
+                <TableCell>
+                  <Button
+                    color="primary"
+                    onClick={(e) => handleOpenEditDialog(e, getBooking)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    color="secondary"
+                    onClick={(e) => handleOpenCancelDialog(e, getBooking)}
+                  >
+                    Cancel
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {/* <TableBody>
             {filteredBookings.map((booking) => (
               <TableRow
                 key={booking.id}
@@ -165,36 +241,41 @@ const AirlineAgentPage = () => {
                 </TableCell>
               </TableRow>
             ))}
-          </TableBody>
+          </TableBody> */}
         </Table>
       </TableContainer>
 
       <Box my={2}>
         <Pagination count={10} color="primary" />
       </Box>
-      {/* Edit Booking Dialog */}
 
+      {/* Edit Booking Dialog */}
       {selectedBooking && (
-        <Dialog open={openEditDialog} onClose={handleCloseEditDialog}>
+        <Dialog 
+          open={openEditDialog} 
+          onClose={handleCloseEditDialog}
+          sx={{ '& .MuiDialog-paper': { 
+            minWidth: '300px',
+            minHeight: '300px' } }}
+        >
           <DialogTitle>Edit Booking</DialogTitle>
           <DialogContent>
             {/* Add form fields for editing the booking */}
             <TextField
               label="Passenger Name"
-              defaultValue={selectedBooking.passengerName}
+              value={putPassenger}
+              onChange={handlePassengerEdit}
+              // defaultValue={selectedBooking.passenger}
               fullWidth
+              InputLabelProps={{
+                style: { paddingTop: '8px' }
+              }}
             />
             {/* Add more fields as needed */}
           </DialogContent>
           <DialogActions>
             <Button onClick={handleCloseEditDialog}>Cancel</Button>
-            <Button
-              onClick={() => {
-                /* Implement save logic here */
-              }}
-            >
-              Save
-            </Button>
+            <Button onClick={handlePutMethod}>Save</Button>
           </DialogActions>
         </Dialog>
       )}
@@ -223,21 +304,26 @@ const AirlineAgentPage = () => {
           </DialogActions>
         </Dialog>
       )}
+
+      {/* Click to show more information */}
       {selectedBooking && (
         <Dialog open={openDialog} onClose={handleCloseDialog}>
           <DialogTitle>Booking Details</DialogTitle>
           <DialogContent>
             <Typography variant="body1">
-              Passenger Name: {selectedBooking.passengerName}
+              Passenger Name: {selectedBooking.passenger}
             </Typography>
-            <Typography variant="body1">
+            <Typography variant="body1" style={{paddingBottom: "2px"}}>
               Flight: {selectedBooking.flight}
             </Typography>
-            <Typography variant="body1">
-              Status: {selectedBooking.status}
+            <Typography variant="body1" style={{paddingBottom: "2px"}}>
+              Status: {selectedBooking.confirm}
             </Typography>
-            <Typography variant="body1">
-              Details: {selectedBooking.details}
+            <Typography variant="body1" style={{paddingBottom: "2px"}}>
+              Seat: {selectedBooking.seat}
+            </Typography>
+            <Typography variant="body1" style={{paddingBottom: "2px"}}>
+              Meal: {selectedBooking.meal}
             </Typography>
           </DialogContent>
           <DialogActions>
